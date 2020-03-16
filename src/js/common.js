073fe1a76a3bd408795a04576809269fefebab10
SergeyG <sergey1008@gmail.com>
2020-03-16 15:58:10 +0500

    бронирование

--- src/js/common.js
+++ src/js/common.js
@@ -16,4 +16,57 @@ document.addEventListener("DOMContentLoaded", function(){
 		},
 		transitionEffect: "slide",
 	});
+
+
 });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	const photosBuySlider = document.querySelector('.slider-full__main .swiper-list');
+	
+	if (!photosBuySlider)
+		return
+
+	import("swiper/js/swiper.esm.js").then(function(Module){
+
+		const {Swiper, Navigation, EffectFade, Thumbs, Keyboard} = Module;
+
+		Swiper.use([Navigation, EffectFade, Thumbs, Keyboard]);
+
+
+
+		new Swiper(photosBuySlider, {
+			effect: "fade",
+			fadeEffect: {
+				crossFade: true
+			},
+
+			keyboard: {
+				enabled: true,
+				onlyInViewport: true
+			},
+			navigation: {
+				nextEl: '.slider-full .swiper-button-next',
+				prevEl: '.slider-full .swiper-button-prev',
+			},
+			thumbs: {
+				swiper: {
+					el: '.slider-full__nav .swiper-list',
+					slidesPerView: 1,
+					// spaceBetween: 0,
+					// watchSlidesVisibility: true,
+					// watchSlidesProgress: true,
+					multipleActiveThumbs: false,
+					loop: false,
+					
+
+				}
+			},
+
+		});
+
+	
+	
+
+	});
+})
