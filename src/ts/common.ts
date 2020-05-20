40eb251291281e71dc4f3dad04a0ec5d46c93f9b
Sergey Gornostaev <sergey1008@gmail.com>
2020-05-20 10:51:54 +0500

    нотификатор и переделал слайдер

--- src/ts/common.ts
+++ src/ts/common.ts
@@ -1,2 +1,14 @@
-import "./forms.ts"
-import "./textPage/text-page.ts"
+import { domReady, App, EventListener } from "./xpage/index";
+import "./forms.ts";
+import "./textPage/text-page.ts";
+import "./notificator.ts";
+
+declare global {
+	interface Window {
+		$: Function;
+		is: any;
+		requestIdleCallback: Function;
+		cancelIdleCallback: Function;
+		notificator: any
+	}
+}
---
+++ src/ts/xpage/index.ts
@@ -0,0 +1,4 @@
+export {default as App} from "./core"
+export {default as Element} from "./Element"
+export {default as EventListener} from "./EventListener"
+export {default as domReady} from "./ready"
