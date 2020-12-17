50af3b38b93fa872a6ab55103f729d75f435a1c9
Sergey <sergey1008@gmail.com>
2020-12-17 17:48:55 +0500

    выгрузил пляжный футбол

--- accesses/accesses.js
+++ accesses/accesses.js
@@ -6,10 +6,10 @@ module.exports = {
         password: '2T1g7E1i4523!2@16'
     },
     server: {
-        path: "/",
-        templatePath: "/",
-        host: "",
-        user: "",
-        password: "",
+        path: "/xn--74-6kcay2ct.xn--p1ai/public_html/local/templates/main2020/",
+        templatePath: "/local/templates/main2020/",
+        host: "87.236.16.158",
+        user: "infosuxr_xpager",
+        password: "vH27%VB9",
     }
 }
