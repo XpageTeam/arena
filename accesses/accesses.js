d4fb565bc25a16e10479bb443259455092b38463
NastyaKhasanova <nastja.bast@yandex.ru>
2020-12-17 10:53:22 +0500

    Начала пляжный футбол

---
+++ accesses/accesses.js
@@ -0,0 +1,15 @@
+module.exports = {
+    xpager: {
+        dirName: "/www/html.xpager.ru/arena/",
+        host: 'html.xpager.ru',
+        user: 'file',
+        password: '2T1g7E1i4523!2@16'
+    },
+    server: {
+        path: "/",
+        templatePath: "/",
+        host: "",
+        user: "",
+        password: "",
+    }
+}
